f2d41ae459c5fc44131a558a51ef112670c8c20c
Fuse23 <ivan.brysyakin@gmail.com>
2024-03-01 02:49:15 +0530

    python server and client

---
+++ client.py
@@ -0,0 +1,120 @@
+import logging
+import time
+import threading
+from argparse import ArgumentParser, Namespace
+
+from diameter.message.constants import *
+from diameter.message.commands.diameter_eap import (
+    Message,
+    DiameterEapRequest,
+    DiameterEapAnswer,
+)
+from diameter.node import Node
+from diameter.node.peer import Peer
+from diameter.node.application import Application, SimpleThreadingApplication
+
+
+# logging.basicConfig(
+#     format="%(asctime)s %(name)-22s %(levelname)-7s %(message)s",
+#     level=logging.DEBUG
+# )
+
+# logging.getLogger("diameter.peer.msg").setLevel(logging.DEBUG)
+
+
+def parse() -> Namespace:
+    parser = ArgumentParser()
+    parser.add_argument(
+        "server_ip",
+        nargs='?',
+        const=1,
+        type=str,
+        help="IP of diameter server",
+    )
+    parser.add_argument(
+        "port",
+        nargs='?',
+        const=1,
+        type=int,
+        help="Port which server listen",
+    )
+    parser.add_argument(
+        "count_of_clients",
+        nargs='?',
+        const=1,
+        type=int,
+        default=5,
+        help="Count of client for create multiple requests",
+    )
+
+    return parser.parse_args()
+
+
+def get_DER(app: Application) -> DiameterEapRequest:
+    message = DiameterEapRequest()
+    message.session_id = app.node.session_generator.next_id()
+    message.origin_host = bytes(app.node.origin_host, "utf-8")
+    message.origin_realm = bytes(app.node.realm_name, "utf-8")
+    message.destination_realm = b"test.realm"
+    message.auth_request_type = 0
+    message.destination_host = b"server.test.realm"
+
+    return message
+
+
+def get_node(number: int) -> Node:
+    return Node(
+        f"client{number}.test.realm",
+        "test.realm"
+    )
+
+
+def get_peer(node: Node, port: int, server_ip: str) -> Peer:
+    return node.add_peer(
+        f"aaa://server.test.realm:{port}",
+        "test.realm",
+        ip_addresses=[server_ip],
+        is_persistent=True,
+    )
+
+
+def create_and_run_node(number: int, port: int, server_ip: str) -> None:
+    node = get_node(number)
+    peer = get_peer(node, port, server_ip)
+
+    app = SimpleThreadingApplication(
+        APP_EAP_APPLICATION,
+        is_auth_application=True
+    )
+
+    node.add_application(app, [peer])
+
+    node.start()
+
+    app.wait_for_ready()
+    answer = app.send_request(get_DER(app))
+
+    print(threading.get_ident(), answer)
+
+    node.stop()
+
+
+def main() -> None:
+    args = parse()
+
+    threads = []
+    for i in range(args.count_of_clients):
+        thread = threading.Thread(target=create_and_run_node, kwargs={
+            "number": i,
+            "port": args.port,
+            "server_ip": args.server_ip,
+        })
+        threads.append(thread)
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+
+if __name__ == "__main__":
+    main()
---
+++ server.py
@@ -0,0 +1,128 @@
+import logging
+import time
+import threading
+from argparse import ArgumentParser, Namespace
+
+from diameter.message.constants import *
+from diameter.message import Message
+from diameter.node import Node
+from diameter.node.peer import Peer
+from diameter.node.application import (
+    SimpleThreadingApplication,
+    Application,
+    ThreadingApplication,
+    _AnyAnswerType,
+)
+
+
+# logging.basicConfig(
+#     format="%(asctime)s %(name)-22s %(levelname)-7s %(message)s",
+#     level=logging.DEBUG
+# )
+
+# logging.getLogger("diameter.peer.msg").setLevel(logging.DEBUG)
+
+
+class MyApp(ThreadingApplication):
+    def handle_request(self, message: Message) -> Message | None:
+        print(f"{threading.get_ident()} Got: {message}")
+        answer = self.generate_answer(
+            message,
+            result_code=E_RESULT_CODE_DIAMETER_SUCCESS
+        )
+
+        return answer
+
+
+def parse() -> Namespace:
+    parser = ArgumentParser()
+    parser.add_argument(
+        "server_ip",
+        nargs='?',
+        const=1,
+        type=str,
+        default="127.0.0.1",
+    )
+    parser.add_argument(
+        "port",
+        nargs='?',
+        const=1,
+        type=int,
+        default=3868,
+    )
+
+    return parser.parse_args()
+
+
+def get_node(ip: str, port: int) -> Node:
+    return Node(
+        origin_host="server.test.realm",
+        realm_name="test.realm",
+        ip_addresses=[ip],
+        tcp_port=port,
+    )
+
+
+def add_peers(node: Node) -> list[Peer]:
+    name_peers = get_peers()
+    peers = []
+
+    for peer in name_peers:
+        peers.append(node.add_peer(peer_uri=peer))
+
+    return peers
+
+
+def get_peers() -> list[str]:
+    peers = []
+
+    for i in range(10):
+        peers.append(f"aaa://client{i}.test.realm")
+
+    return peers
+
+
+def handle_request(app: Application, message: Message) -> _AnyAnswerType:
+    print(f"{threading.get_ident()} Got: {message}")
+    # logging.info()
+    answer = app.generate_answer(
+        message,
+        result_code=E_RESULT_CODE_DIAMETER_SUCCESS
+    )
+    return answer
+
+
+def get_app() -> Application:
+    # return SimpleThreadingApplication(
+    #     APP_EAP_APPLICATION,
+    #     is_auth_application=True,
+    #     max_threads=5,
+    #     request_handler=handle_request,
+    # )
+    return MyApp(
+        APP_EAP_APPLICATION,
+        is_auth_application=True,
+        max_threads=10,
+    )
+
+
+def main() -> None:
+    args = parse()
+
+    node = get_node(args.server_ip, args.port)
+    peers = add_peers(node)
+    app = get_app()
+    node.add_application(app, peers)
+
+    node.start()
+
+    try:
+        while True:
+            time.sleep(1)
+
+    except (KeyboardInterrupt, SystemExit) as e:
+        node.stop()
+
+
+if __name__ == "__main__":
+    main()
